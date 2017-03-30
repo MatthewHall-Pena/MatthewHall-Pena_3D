4c996b886c385a18dd13d03ed84ce2f4e9325cd9
MatthewHall-Pena <mhallpena@stuy.edu>
2017-03-30 02:04:10 -0400

    33

--- parser.c
+++ parser.c
@@ -91,11 +91,38 @@ void parse_file ( char * filename,
     double yvals[4];
     double zvals[4];
     struct matrix *tmp;
-    double r;
+    double r,r2;
     double theta;
     char axis;
     int type;
     double step = 0.01;
+
+	if ( strncmp(line, "box", strlen(line)) == 0 ) {
+      fgets(line, sizeof(line), f);
+      //printf("BOX\t%s", line);
+      
+      sscanf(line, "%lf %lf %lf %lf %lf %lf",
+	     xvals, yvals, zvals, xvals+1, yvals+1, zvals+1);
+      add_box( edges, xvals[0], yvals[0], zvals[0], xvals[1], yvals[1], zvals[1]);
+    }//end of box
+
+    else if ( strncmp(line, "sphere", strlen(line)) == 0 ) {
+      fgets(line, sizeof(line), f);
+      //printf("SPHERE\t%s", line);
+      
+      sscanf(line, "%lf %lf %lf %lf",
+	     xvals, yvals, zvals, &r);
+      add_sphere( edges, xvals[0], yvals[0], zvals[0], r, step );
+    }//end of sphere
+
+    else if ( strncmp(line, "torus", strlen(line)) == 0 ) {
+      fgets(line, sizeof(line), f);
+      //printf("TORUS\t%s", line);
+      
+      sscanf(line, "%lf %lf %lf %lf %lf",
+	     xvals, yvals, zvals, &r, &r2);
+      add_torus( edges, xvals[0], yvals[0], zvals[0], r, r2, step );
+    }//end of torus
 
     if ( strncmp(line, "circle", strlen(line)) == 0 ) {
       fgets(line, sizeof(line), f);
